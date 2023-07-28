a59b1564846f746179dbdecbf0ffca23e6d5089e
Maria Granell Ruiz <mgranellruiz@gmail.com>
2023-07-28 16:24:15 +0200

    first version working

--- faecal/faecalShiny/app.R
+++ faecal/faecalShiny/app.R
@@ -17,26 +17,43 @@ library(ggthemr)
 library(stringr)
 library(readxl)
 library(waffle)
+#library(shinydashboard)
 
-# Load the data and perform necessary preprocessing
+# library ---------------------
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+library(ggplot2)
+library(lubridate)
+library(ggthemr)
+library(readxl)
+library(stringr)
+library(waffle)
+
+# path ------------------------
+setwd("/Users/mariagranell/Repositories/hormones/hormone_fecal")
+
 dfield <- read.csv("/Users/mariagranell/Repositories/hormones/hormone_fecal/crybertrackert_2023-07-16.CSV")
 
-# ... (rest of the data preprocessing code, as it is in your script)
+dfield <- dfield %>%
+  mutate(Date = ymd(Date),
+         WBS = as.numeric(str_replace_all(WBS, ";",".")),
+         Time.1 = as.numeric(str_replace_all(Time.1, ";",":")),
+         SampleNb = as.numeric(SampleNb),
+         Collector = if_else(Collector == '', 'NA', Collector),
+         ManualTime = str_replace_all(Time.1, ";",":")) %>%
+  filter(!is.na(Group), Group != "Migrating/lone")
 
-# Define global variables for colors
-ggthemr('solarized', type = 'outer', layout = "minimal", spacing =2)
+# plot colours --------
+ggthemr('pale', type = 'outer', layout = "minimal", spacing =2)
+swatch()
 gray_solarized <- "#073642"
-blue_solarized <- "#268bd2"
-red_solarized <- "#dc322f"
+blue_solarized <- "#3262ab"
+red_solarized <- "#eb9050"
 
-## HORMONES DATA ----------------
-dfield_h <- dfield %>%
-  filter(SampleType == "Hormones") %>%
-  mutate(
-    WBS = as.numeric(str_replace_all(WBS, ";",".")),
-    ManualTime = str_replace_all(Time.1, ";",":")
-  ) %>%
-  select(-c("SampleType", "GPSS","GPSE","Observers","DataInfo","Remarks","DeviceId","Data", "Time.1"))
+## HORMONES DATA TO MERGE----------------
+
+#TODO ERRORS -------------
+# sample 243 appears twice in dlab
 
 #data collected in the lab
 dlab <- read_excel("/Users/mariagranell/Repositories/hormones/hormone_fecal/fecal_hormones_11.06.23.xlsx")
@@ -45,30 +62,33 @@ dlab <- dlab %>% select(-c("corrected_process_date", "...9")) %>% # ignore warni
          was = as.numeric(was),
          total = as.numeric(total),
          process_date = ymd(process_date),
-         dry_weight = as.numeric(dry_weight))
+         SampleType = "Hormones",
+         dry_weight = as.numeric(dry_weight)) %>%
+  filter(replica == 1) %>%
+  # removing of errors
+  filter(n_sample != 243)
 
 #combination of both data sets
-dh <- merge(dlab,dfield_h, by.x="n_sample", by.y = "SampleNb")
+dfield_h <- left_join(dfield, dlab, by = c( "SampleNb" = "n_sample", "SampleType" = "SampleType"))
 
 #I will confider appropiate if it desviates 20% of 0.4, i.e. 0.08, but I will up it to 0.1.
 #Thus a proper value is between 0.5 and 0.3
-dh$appropiate_amount <- ifelse( between(dh$total, 0.3, 0.5), "Appropiate", "Inappropiate")
+dfield_h$appropiate_amount <- ifelse(between(dfield_h$total, 0.3, 0.5), "Appropiate", "Inappropiate")
 
-dh<-dh %>%
-  mutate(Collector = if_else(Collector == '', 'NA', Collector)) %>%
-  filter(!is.na(Group))
-dh$total <-dh$was - dh$wbs
+# GENETICS DATASET
+d_genetics <-  read_excel("/Users/mariagranell/Desktop/IVP/Genetic_samples_overview.xlsx")
 
-# percentage of appropiates --------------
-d_percentage <- dh %>%
-  group_by(Group) %>%
-  filter(!is.na(appropiate_amount)) %>%
-  summarise(percentage_appropriate = (sum(appropiate_amount == "Appropiate")/n())*100)
+d_genetics <- d_genetics %>% mutate(Complete = na_if(Complete, "Complete?"))
+d_genetics$Complete <- ifelse(is.na(d_genetics$Complete), "No", d_genetics$Complete)
+genetics_plot <- d_genetics %>%
+  group_by(Group, Complete) %>%
+  summarise(number= as.numeric(n()))
 
-dh <- left_join(dh, d_percentage, by = "Group")
 
 # Define the UI for the Shiny app
 ui <- fluidPage(
+    theme = bslib::bs_theme(bootswatch = "united"),
+    #dashboardHeader(title = "Poop lab progress"),
   titlePanel("Poop Data Analysis"), # App title
 
   # Sidebar layout with input widgets
@@ -82,118 +102,151 @@ ui <- fluidPage(
       )
     ),
 
+    checkboxGroupInput(inputId = "group_filter",
+                         label = "Select Group(s) to include:",
+                         choices = unique(dfield_h$Group),
+                         selected = unique(dfield_h$Group))
+    ),
+
     # Main panel to display the plot
     mainPanel(
       # Output: Plot 1
-      plotOutput(outputId = "poop_plot"),
+      plotOutput(outputId = "hormones_histogram"),
 
       # Output: Plot 2
-      plotOutput(outputId = "group_plot")
+      plotOutput(outputId = "hormones_sex"),
+
+      # Output: Plot 3
+      plotOutput(outputId = "edna_sex"),
+
+      # Output: Plot 4
+      plotOutput(outputId = "genetics_waffle")
     )
   )
-)
+
 
 # Define the server logic for the Shiny app
 server <- function(input, output) {
 
   # Reactive function for filtering the data based on the date range input
-  filtered_data <- reactive({
-    data_filtered <- dh %>%
+  filtered_dfield_h <- reactive({
+    dfield_h %>%
       filter(Date >= input$date_range[1],
-             Date <= input$date_range[2])
-
-    d_percentage <- data_filtered %>%
-      group_by(Group) %>%
-      filter(!is.na(appropiate_amount)) %>%
-      summarise(percentage_appropriate = (sum(appropiate_amount == "Appropiate") / n()) * 100)
-
-    left_join(data_filtered, d_percentage, by = "Group")
-  })
-
-  percentage_labels <- reactive({
-    filtered_data() %>%
-      group_by(Group) %>%
-      filter(!is.na(appropiate_amount)) %>%
-      summarise(percentage_appropriate = (sum(appropiate_amount == "Appropiate") / n()) * 100)
+             Date <= input$date_range[2],
+             Group %in% input$group_filter)
 
   })
 
-  eDNAfilter <- reactive({
-    eDNAfilter <- dfield_e %>%
-      filter(Date >= input$date_range[1],
-             Date <= input$date_range[2])
+  filtered_genetics_plot <- reactive({
+    genetics_plot %>%
+      filter(Group %in% input$group_filter)
   })
 
   # Reactive function to generate the plot based on the filtered data
-  output$poop_plot <- renderPlot({
-    filtered_data() %>%
-      ggplot(., aes(total, fill = appropiate_amount)) +
-      geom_histogram(na.rm = TRUE, bins = 100) +
+  output$hormones_histogram <- renderPlot({
+    filtered_dfield_h() %>%
+      filter(!is.na(appropiate_amount)) %>%
+      ggplot(.,aes( total ,fill = appropiate_amount)) +
+      geom_histogram(na.rm =T, bins = 100) +
       facet_grid(Group ~ ., switch = "y") +
-      geom_vline(xintercept = 0.4, color = gray_solarized, linetype = "dashed", size = 1) +
-      #geom_text(data = percentage_labels(), aes(x = total, y = 12, label = paste0(round(percentage_appropriate, 0), "%")),
-      #          vjust = 2, hjust = 0.5, size = 4, color = gray_solarized) +
+      geom_vline(xintercept = 0.4, color = "#073642", linetype = "dashed", size = 1) +
+  #geom_text(data = d, aes(x = 2, y = 12, label = paste0(round(percentage_appropriate, 0), "%")),
+  #          vjust = 2, hjust = 0, size = 4, color = "#073642") +
+  # Add your customizations below
       labs(x = "Weight of a sample",
            y = "Number of hormone samples",
            fill = "Are we collecting the right amount of poop?",
-           title = "The dotted line represents a Weight of >= 0.4, i.e. the appropriate amount\nand the % is the amount of correct hormones poop collected",
-           caption = NULL
+           title = "The dotted line represents a Weight of >= 0.4, i.e. the appropiate amount
+           and the % is the amount of correct hormones poop collected"
       ) +
+      #scale_y_discrete(position = "left") +
       theme(
+        # Adjust the appearance of axis text
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_text(size = 12, face = "bold"),
         legend.position = "top",
+        #plot.caption = element_text(size = 10, hjust = 0.5),
         plot.title = element_text(size = 10, hjust = 0.5, vjust = -1, face = "plain"),
-        legend.title = element_text(size = 15, face = "bold")
+        legend.title = element_text(size = 15, face = "bold"),
       )
   })
 
   # Reactive function to generate Plot 2 based on the filtered data
-  output$group_plot <- renderPlot({
-    filtered_data() %>%
-      group_by(Group, Sex) %>%
+  output$hormones_sex <- renderPlot({
+    filtered_dfield_h() %>%
+      filter(!is.na(appropiate_amount)) %>%
+      group_by(Group, Sex)%>%
       tally() %>%
-      ggplot(aes(x = Group, y = n, fill = Sex)) +
-      geom_col(position = "dodge") +
-      geom_text(aes(label = n), position = position_dodge(width = 0.9), hjust = -0.1, vjust = 0.5) +
+      ggplot(aes(x=Group, y=n, fill=Sex))+
+      geom_col(position = "dodge")+
+      geom_text(aes(label = n), position = position_dodge(width = 0.9),  hjust = 1.2, vjust = 0.5, colour = "white") +
       ylab("Number of fecal samples") +
       coord_flip() +
       labs(y = "Number of hormone samples",
            title = "Fecal hormone samples by Group"
       ) +
       theme(
-        plot.title = element_text(hjust = 0.5),
-        legend.title = element_text(size = 15, face = "bold")
+      plot.title = element_text(hjust = 0.5),
+      legend.title = element_text(size = 15, face = "bold")
       ) +
       scale_fill_manual(values = c("Female" = red_solarized, "Male" = blue_solarized))
+
   })
 
   ### EDNA -----------
 
   # maria estas aqui
-  output$group_plot <- renderPlot({
-    filtered_data() %>%
-  plot_edna <- dfield_e%>%
-  group_by(Group, Sex)%>%
-  tally() %>%
-  filter(Sex == "Female" | Sex == "Male" & Group != "Migrating/lone") %>%
-  ggplot(aes(x=Group, y=n, fill=Sex))+
-  geom_col(position = "dodge")+
-  geom_text(aes(label = n), position = position_dodge(width = 0.9), hjust = -0.1, vjust = 0.5) +
-  ylab("Number of eDNA samples") +
-  coord_flip() +
-  labs(y = "Number of eDNA samples",
+  output$edna_sex <- renderPlot({
+    filtered_dfield_h() %>%
+    filter(SampleType == "eDNA") %>%
+    group_by(Group, Sex)%>%
+    tally() %>%
+    filter(Sex == "Female" | Sex == "Male") %>%
+    ggplot(aes(x=Group, y=n, fill=Sex))+
+    geom_col(position = "dodge")+
+    geom_text(aes(label = n), position = position_dodge(width = 0.9), hjust = 1.2, vjust = 0.5, colour = "white") +
+      ylab("Number of eDNA samples") +
+    coord_flip() +
+    labs(y = "Number of eDNA samples",
        title = "Fecal eDNA samples by Group"
-  ) +
+    ) +
     theme(
     plot.title = element_text(hjust = 0.5),
     legend.title = element_text(size = 15, face = "bold")
-  ) +
-  scale_fill_manual(values = c("Female" = red_solarized, "Male" = blue_solarized))
+    ) +
+    scale_fill_manual(values = c("Female" = red_solarized, "Male" = blue_solarized))
 
 })
+
+### GENETICS WAFFLE
+
+  output$genetics_waffle <- renderPlot({
+    filtered_genetics_plot() %>%
+    ggplot(., aes(fill=Complete, values=number)) +
+  geom_waffle(color = "white", n_rows = 6, na.rm = TRUE) +
+  facet_wrap(~Group, ncol=4) +
+  #geom_text(aes(x = 9, y = 5, label = paste0(prop_yes, "/", all)), size = 4, color = gray_solarized) +
+  scale_x_discrete(expand=c(0,0)) +
+  scale_y_discrete(expand=c(0,0)) +
+  coord_equal() +
+  labs(
+    x = "",
+    y = "",
+    title = "Number of individuals with Genetics completed",
+    subtitle = "The number represent how many individuals are NOT completed / total"
+  ) +
+  theme (
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    axis.line = element_blank(),
+    axis.text.x = element_blank()
+  ) +
+  scale_fill_manual(values = c("No" = red_solarized, "Yes" = blue_solarized))
+
+})
+}
 
 # Run the Shiny app
 shinyApp(ui = ui, server = server)
